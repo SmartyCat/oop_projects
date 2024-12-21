46a607dfdb57546cf852c189f5048ce619cd99f5
Kostya_B <beztalannyjkonstantin@gmail.com>
2024-12-21 17:02:42 +0200

    New compoz.py with coments

--- compoz.py
+++ compoz.py
@@ -1,28 +1,47 @@
+"""Модуль для вычесления площади комнаты и количества рулонов обоев"""
+
+
 class WinDoor:
+    """Класс окно/дверь"""
+
     def __init__(self, x, y):
+        """Вычесление площади окна/двери"""
+
         self.square = x * y
 
 
 class Room:
+    """Класс комната"""
+
     def __init__(self, w, l, h):
+        """Инициализатор принимает ширину высоту и длинну"""
+
         self.width = w
         self.length = l
         self.height = h
         self.wd = []
 
     def get_all_square(self):
+        """Вычесления площади всей комнаты"""
+
         return 2 * self.height * (self.width + self.length)
 
     def add_wd(self, x, y):
+        """Добавление площадей окон или дверей"""
+
         self.wd.append(WinDoor(x, y))
 
     def get_new_square(self):
+        """Вычесление нового площади комнаты с учетом вычета площадей дверей и окон"""
+
         new_square = self.get_all_square()
         for i in self.wd:
             new_square -= i.square
         return new_square
 
     def get_count_wallpapers(self, l, w):
+        """Функция вычесления количества рулонов"""
+
         check = self.get_new_square()
         counter = 0
         while check > 0:
