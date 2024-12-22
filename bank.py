d80338a29f650fabac8ce8079b0fc3fc36d11f92
Kostya_B <beztalannyjkonstantin@gmail.com>
2024-12-22 19:22:37 +0200

    Новые изменения плюс документация

--- bank.py
+++ bank.py
@@ -1,44 +1,56 @@
 class BankAccount:
+    """Класс для аккаунта банка"""
 
     def __init__(self, owner, balance=0):
+        """Инициализация имени владельца и балана"""
         self.history = []
         self.owner = owner
         self.check_balnce(balance)
         self.balance = balance
 
     def deposite(self, amount):
+        """Метод для пополнени счета"""
         if amount <= 0:
             raise ValueError("You need to use ntegers bigger than 0")
         self.balance += amount
         self.history.append(f"Deposited {amount}")
 
     def withdraw(self, amount):
+        """Метод для снятия счета"""
         self.check_amount(self.balance, amount)
         self.balance -= amount
         self.history.append(f"Withdraw {amount}")
 
     def transfer(self, amount, other_account):
+        "Метод для первода денег"
         self.check_amount(self.balance, amount)
         other_account.balance += amount
         self.balance -= amount
         self.history.append(f"Transfer {amount} on {other_account.owner}")
 
     def apply_interest(self, p):
-        if isinstance(p, (int, float)) is not True or p < 0:
+        """Метод начисления процентов"""
+        if not isinstance(p, (int, float)) or p < 0:
             raise ValueError
         interest = self.balance * p
+
+        self.check_balance(self.balance + interest)
         self.balance += interest
+        self.history.append(f"Applied interest rate: {p}")
 
     def show_history(self):
+        """Метод для отобржения истории"""
         return self.history
 
     @staticmethod
     def check_amount(dep, amount):
+        """Метод для проверки счета"""
         if dep < amount:
             raise ValueError("Amount must ve less than balance")
 
     @staticmethod
     def check_balance(b):
+        """Метод для проверки баланса"""
         if b > 1000000:
             raise ValueError("you cant keep over milion")
         elif b < 0:
