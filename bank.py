9f46b96544e305cb4d74b8eeded5e435853282ff
Kostya_B <beztalannyjkonstantin@gmail.com>
2024-12-22 16:51:59 +0200

    task №2 22.12.24

---
+++ bank.py
@@ -0,0 +1,26 @@
+class BankAccount:
+    def __init__(self, owner, balance=0):
+        self.owner = owner
+        self.balance = balance
+
+    def deposite(self, amount):
+        if amount <= 0:
+            raise ValueError("You need to use ntegers bigger than 0")
+        self.balance += amount
+
+    def withdraw(self, amount):
+        self.check_amount(self.balance, amount) is not True
+        self.balance -= amount
+
+    def transfer(self, amount, other_account):
+        self.check_amount(self.balance, amount)
+        other_account.balance += amount
+        self.balance -= amount
+
+    @staticmethod
+    def check_amount(dep, amount):
+        if dep <= amount:
+            raise ValueError("Amount must ve less than balance")
+
+    def __str__(self):
+        return f"{self.owner}, {self.balance}"
