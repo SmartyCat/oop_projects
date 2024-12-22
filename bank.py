3f5d3e6b30503bd435385e1566c52bb334912c06
Kostya_B <beztalannyjkonstantin@gmail.com>
2024-12-22 18:41:37 +0200

    улучшения банка

--- bank.py
+++ bank.py
@@ -1,26 +1,48 @@
 class BankAccount:
-    def __init__(self, owner, balance=0):
+
+    def __init__(self, owner, balance=0, percent=0):
+        self.history = []
         self.owner = owner
+        self.check_balnce(balance)
         self.balance = balance
+        self.apply_interest(percent)
+        self.percent = percent
 
     def deposite(self, amount):
         if amount <= 0:
             raise ValueError("You need to use ntegers bigger than 0")
         self.balance += amount
+        self.history.append(f"Deposited {amount}")
 
     def withdraw(self, amount):
         self.check_amount(self.balance, amount)
         self.balance -= amount
+        self.history.append(f"Withdraw {amount}")
 
     def transfer(self, amount, other_account):
         self.check_amount(self.balance, amount)
         other_account.balance += amount
         self.balance -= amount
+        self.history.append(f"Transfer {amount} on {other_account.owner}")
+
+    def apply_interest(self, p):
+        if isinstance(p, (int, float)) is not True or p < 0:
+            raise ValueError
+        interest = self.balance * p
+        self.balance += interest
+
+    def show_history(self):
+        return self.history
 
     @staticmethod
     def check_amount(dep, amount):
         if dep < amount:
             raise ValueError("Amount must ve less than balance")
+
+    @staticmethod
+    def check_balance(b):
+        if b > 1000000:
+            raise ValueError("you cant keep over milion")
 
     def __str__(self):
         return f"{self.owner}, {self.balance}"
