d664b2fc3ca464e9ae7bfc3d015cc579e3204971
Kostya_B <beztalannyjkonstantin@gmail.com>
2025-01-28 17:42:06 +0200

    correct

--- HourClock.py
+++ HourClock.py
@@ -1,14 +1,24 @@
 class HourClock:
-    def __init__(self,hours):
-        if isinstance(hours,int) and 0<hours<13:
-            self.__hours=hours
+    def __init__(self, hours):
+        if self.check(hours):
+            self.__hours = hours
         else:
             raise ValueError("Некорректное время")
-    
+
+    @staticmethod
+    def check(x):
+        if isinstance(x, int) and 1 <= x <= 12:
+            return True
+        else:
+            return False
+
     @property
     def hours(self):
         return self.__hours
-    @hours.setter
-    def hours(self,h):
-        self.__hours=h
 
+    @hours.setter
+    def hours(self, h):
+        if self.check(h):
+            self.__hours = h
+        else:
+            raise ValueError("Некоректное время")
