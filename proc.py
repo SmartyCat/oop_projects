a673f38fe5548a14749216cb9e5e76ef4dd055ac
Kostya_B <beztalannyjkonstantin@gmail.com>
2025-02-01 17:07:01 +0200

    zaebalsa

--- proc.py
+++ proc.py
@@ -4,25 +4,23 @@ from functools import singledispatchmethod
 class Processor:
 
     @singledispatchmethod
-    def process(x):
-        raise TypeError("Eror")
+    def process(self,x):
+        raise TypeError("неподдерживаемый файл")
 
     @process.register(int)
     @process.register(float)
-    def int_float_proces(x):
+    def int_float_proces(self,x):
         return x * 2
 
     @process.register(str)
-    def str_proces(x):
+    def str_proces(self,x):
         return x.upper()
 
     @process.register(list)
-    def list_proces(x):
+    def list_proces(self,x):
         return x[::-1]
 
     @process.register(tuple)
-    def tuple_proces(x):
+    def tuple_proces(self,x):
         x = sorted(x, reverse=True)
         return tuple(x)
-
-print("xuy")
