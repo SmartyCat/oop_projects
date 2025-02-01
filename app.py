292d8df70ecf9ede6e752bb1196ac661843135e3
Kostya_B <beztalannyjkonstantin@gmail.com>
2025-02-01 15:23:56 +0200

    тренировка авторизации

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, g, redirect, render_template, request
+from flask import Flask, redirect, render_template, g, request
 import sqlite3 as sq
-from flask_login import LoginManager, login_required
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import (
     LoginManager,
@@ -9,21 +8,21 @@ from flask_login import (
     login_user,
     logout_user,
 )
-from UserLogin import UserLogin, UserMixin
+from UserLogin import UserLogin
 
-DATABASE = "users.db"
-SECRET_KEY = "1234qwer"
+DATABASE = "user.db"
+DEBUG = True
+SECRET_KEY = "1234QWER"
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-
 login_manager = LoginManager(app)
+login_manager.login_view = "login"
 
 
 @login_manager.user_loader
 def load_user(user_id):
     db = get_db()
-    print("xuy")
     return UserLogin().fromDB(user_id, db)
 
 
@@ -54,53 +53,51 @@ def close_db(error):
 
 @app.route("/")
 def index():
-    if current_user.is_authenticated:
-        return redirect("profile")
     return render_template("index.html")
 
 
 @app.route("/registr", methods=["POST", "GET"])
 def registr():
     db = get_db()
+    if not current_user:
+        return redirect("/profile")
     if request.method == "POST":
-        if request.form.get("email") and request.form.get(
-            "password"
-        ) == request.form.get("password2"):
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if username and password == request.form.get("password2"):
             db.cursor().execute(
                 "INSERT INTO users VALUES(Null,?,?)",
-                (
-                    request.form.get("email"),
-                    generate_password_hash(request.form.get("password")),
-                ),
+                (username, generate_password_hash(password)),
             )
             db.commit()
+            return redirect("/")
     return render_template("registr.html")
-
-
 
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
     db = get_db()
+    if not current_user:
+        return redirect("profile")
     if request.method == "POST":
-        email = request.form.get("email_login")
-        user = (
-            db.cursor()
-            .execute("SELECT * FROM users WHERE email=? LIMIT 1", (email,))
-            .fetchone()
-        )
+        user = db.cursor().execute(
+            "SELECT * FROM users WHERE username=?",
+            (request.form.get("username_login"),),
+        ).fetchone()
         if user and check_password_hash(user[2], request.form.get("password_login")):
             userlogin = UserLogin().create(user)
-            login_user(userlogin)
-            return redirect("/")
+            rm = True if request.form.get("remember") else False
+            login_user(userlogin, remember=rm)
+            return redirect("/profile")
     return render_template("login.html")
 
 
 @app.route("/profile")
+@login_required
 def profile():
-    db = get_db()
-    email=db.cursor().execute("SELECT email FROM users WHERE id = ? LIMIT 1",(current_user.get_id(),)).fetchone()
-    return render_template("profile.html",email=email[0])
+    db=get_db()
+    n=db.cursor().execute("SELECT username FROM users WHERE id = ? LIMIT 1",(current_user.get_id())).fetchone()
+    return render_template("profile.html",name=n[0])
 
 
 @app.route("/logout")
