c863bd27a63a4a6b56a022e10ab24ab77e9ccf35
Kostya_B <beztalannyjkonstantin@gmail.com>
2025-02-01 16:52:14 +0200

    new oop task

---
+++ proc.py
@@ -0,0 +1,26 @@
+from functools import singledispatchmethod
+
+
+class Processor:
+
+    @singledispatchmethod
+    def process(x):
+        raise TypeError("Eror")
+
+    @process.register(int)
+    @process.register(float)
+    def int_float_proces(x):
+        return x * 2
+
+    @process.register(str)
+    def str_proces(x):
+        return x.upper()
+
+    @process.register(list)
+    def list_proces(x):
+        return x[::-1]
+
+    @process.register(tuple)
+    def tuple_proces(x):
+        x = sorted(x, reverse=True)
+        return tuple(x)
